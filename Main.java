55e19a025d46740c88bd1d810c805478fe0a6710
ramcj7742 <ramcj7742@wrdsb.ca>
2020-10-14 18:14:40 +0000

    finished astericks problem

--- Main.java
+++ Main.java
@@ -1,15 +1,36 @@
+import java.util.Scanner;
 /**
- *
- * @author 
+ * prints random amount of astericks between 1 and 5
+ * @author Jaden Ramcharan
  */
 public class Main {
+  //prints astericks
+  public static void chaotic(int userNum){
+    //gets first random number
+    int randNum = (int)(Math.random()*(5 - 1 + 1))+ 1;
+    for(int i = 0; i < userNum; i++){
+      for(int j = 0; j < randNum; j++){
+        //prints the astericks on same line
+        System.out.print("*");
+      }
+    //chooses new amount of asterics and goes to next line
+     randNum = (int)(Math.random()*(5 - 1 + 1))+ 1;
+     System.out.println("");
+    }
+  }
 
   /**
    * The method that is executed when you hit the run button.
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
-    
+    // creates scanner for user input
+    Scanner input = new Scanner(System.in);
+
+    //gets number from user
+    int userNum = input.nextInt();
+
+    //prints astericks
+    chaotic(userNum);
   }
 }
